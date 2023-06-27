f78c5545b26f066acc3a0bf8faae004022bab913
harjas816 <harjas816@gmail.com>
2023-06-27 12:21:05 -0400

    added front and back functionality to card

--- tasty-bite-site/src/components/RecipeCard.js
+++ tasty-bite-site/src/components/RecipeCard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const RecipeCard = ({title, image, time, ingredients, instructions}) => {
+
+    const [card, setCard] = useState("true")
+
+    function switchCard(){
+        setCard(!card)
+    }
+
+    const front = <div onClick={switchCard}><img src = {image} height={300} width={300} alt = "food"></img><p className="title">{title}</p></div>
+
+    const back = <div onClick={switchCard}><p className="ingredients">{ingredients}</p><p className="instructions">{instructions}</p><p>{time}</p></div>
+   
     return(
         <div className="recipe-card">
-            <img src={image} alt="food" height={300} width={300}></img>
-            <p className="title">{title}</p>
-            <p className="time">{time}</p>
-            <p className="ingredients">{ingredients}</p>
-            <p className="instructions">{instructions}</p>
+            {card ? [front]: [back]}
+            <button></button>
         </div>
     )
 }
