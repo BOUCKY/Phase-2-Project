39b2d75051499d123b09930b3aa8495025213bd0
Kevin Mrozek <129324133+5billon@users.noreply.github.com>
2023-06-27 14:17:01 -0500

    Update AddRecipe.js

--- tasty-bite-site/src/components/AddRecipe.js
+++ tasty-bite-site/src/components/AddRecipe.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
+const initialState = {
+    category: '',
+    title: '',
+    image: '',
+    ingredients: '',
+    instructions: '',
+}
+const AddRecipe = ({setFoodData}) => {
+    const [formData, setFormData] = useState(initialState)
 
-const AddRecipe = () => {
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        fetch('http://localhost:3000/recipes', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(r => r.json())
+            .then((newRecipes) => {
+                setFoodData((existingRecipes) => [...existingRecipes, newRecipes])
+                setFormData(initialState)
+            })
+    }
+    function handleChange(e) {
+        console.log(e.target)
+        setFormData((currentFormData) => ({...currentFormData, [e.target.name]: e.target.value}))
+    }
     return(
-        <div className="addRecipe">
-        
+        <div>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                id='category'
+                name='category'
+                placeholder="category"
+                value={formData.category}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                id='title'
+                name='title'
+                placeholder="title"
+                value={formData.title}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                id='image'
+                name='image'
+                placeholder="image"
+                value={formData.image}
+                onChange={handleChange}
+            />
+            <input
+                type="number"
+                id='time'
+                name='time'
+                placeholder="time"
+                value={formData.time}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                id='ingredients'
+                name='ingredients'
+                placeholder="ingredients"
+                value={formData.ingredients}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                id='instructions'
+                name='instructions'
+                placeholder="instructions"
+                value={formData.instructions}
+                onChange={handleChange}
+            />
+            <button type='submit'>Submit Recipe!</button>
+        </form>
         </div>
     )
 }
